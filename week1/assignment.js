5fa169ebca9d83f5ef83c891ff326a5d5fabfc44
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-13 07:10:19 +0000

    assign done

--- week1/assignment.js
+++ week1/assignment.js
@@ -3,54 +3,47 @@
 Create a function that takes an array of objects as input and returns the users whose age > 18 and are male           */
 
 
+function vote_and_male(array){
+    return array.filter(person=>person.age>=18  &&  person.gender==="male")
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//**************       ARRAY of OBJECTS */
-
+/* *************       ARRAY of OBJECTS       ************* */
 const users=[
 
     {
         name:"buttan",
-        gender:male,
+        gender:"male",
         age:45
     },
 
     {
         name:"sarkar",
-        gender:male,
+        gender:"male",
         age:89
     },
 
     {
         name:"khushi",
-        gender:female,
+        gender:"female",
         age:20
     },
 
     {
         name:"aaranya",
-        gender:female,
+        gender:"female",
         age:19
     },
 
     {
         name:"aditya",
-        gender:male,
+        gender:"male",
         age:22
     } 
 ]
+
+const result= vote_and_male(users)
+console.log(result)
 
 
 
