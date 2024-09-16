adfb81fdff6d857991c8c9018d0ba2851d8148c7
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-16 09:24:42 +0000

    week2.1 class done

--- week2/classwork1.js
+++ week2/classwork1.js
@@ -51,6 +51,7 @@
 
 //simple calculator
 
+/*
 function add(a,b){
     return a+b
 }
@@ -67,12 +68,79 @@ function divide(a,b){
     return a/b
 }
 
-
 function do_operation(num1,num2,operation){
     return operation(num1,num2)
 }
 
 let result=do_operation(95,5,divide)
 console.log(result)
+*/
+
+
+
+
+//*********     Asynchronous code and callbacks     ************************/
+
+
+/*
+
+const libr=require("fs")
+
+function print(err,data){             //known as error first function
+    console.log(data)
+}
+libr.readFile("week2/a1.txt", "utf-8", print)
+libr.readFile("week2/b1.txt", "utf-8", print)
+console.log("done dana dan")
+
+*/
+
+
+
+// function readFile(filepath, encoding, op){      //op --> ya fir callback
+//     //readfile
+//     op("error","file_data")   //handle error pahle of this file then do print of data....
+// }
+
+
+////////////writing as actual function of print function..
+
+// const libr=require("fs")
+
+// function print(err,data){ 
+//     if(err){
+//         console.log("galat hai brooooooooo" , err)
+//     }else{           
+//     console.log("data is " ,data)
+//     }
+// }
+// libr.readFile("week2/a1.txt", "utf-8", print)
+// libr.readFile("week2/b1.txt", "utf-8", print)
+// console.log("done dana dan")
+
+
+
+
+console.log("helllllllloooooooo")
+
+function timeout(){
+    console.log("asynch call timeout")
+}
+
+setTimeout(timeout,10000)
+
+console.log("hi there...")
+
+let c=0
+for(let i=0; i<20; i++){
+    c=c+1
+}
+console.log(c)
+
+//ab yaha pe callback queue se call stack me jayege api request --->  settimeout...
+
+
+
+
 
 
