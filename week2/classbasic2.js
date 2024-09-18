d4939aff9a99e8b31cead67f00dec5c42713d3e5
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-18 03:09:44 +0000

    class in js basics

---
+++ week2/classbasic2.js
@@ -0,0 +1,49 @@
+// class rectangle{
+
+//     constructor(height,width,color){
+//         this.height=height
+//         this.width=width
+//         this.color=color
+//     }
+
+//     area(){
+//         return this.height*this.width
+//     }
+
+//     color(){
+//         console.log(`color the rectangle with ${this.color}`)
+//     }
+
+// }
+
+
+// const obj=new rectangle(5,10,"blue");
+// // obj.color();
+// //console.log(obj.color)
+// console.log(obj.area())
+
+
+//*************************************************************************************************************** */
+// NOTE --> the difference b/w old and new syntax of making object is that in the old we just storing value in a key value
+// pair but in the new method we makes a class of the rectangle in which we stored the proprerty of rectangle in a constructor...
+//and also give access to attach function in a obj
+
+//old school :)
+
+// const rect={
+//     width:10,
+//     height:5,
+//     color:"blue"
+// }
+
+// console.log(rect.width)
+// console.log(rect.color)
+
+
+//**********  MAP */
+
+// const map=new Map()
+// map.set('Name','buttan')
+// map.set('age',52)
+
+// console.log(map.get('Name'));
